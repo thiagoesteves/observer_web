fa0dc11af3fc53457d039d6d76c85533490480b3
Thiago Esteves <calori@gmail.com>
2025-02-04 15:36:23 -0300

    g

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ObserverWeb.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/thiagoesteves/observer_web"
-  @version "0.0.0"
+  @version "0.0.1"
 
   def project do
     [
